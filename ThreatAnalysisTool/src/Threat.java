1bee00d87962cb976593e72837fbbd34635e6cc5
kopecs48 <shanekopec123@gmail.com>
2021-10-09 18:48:11 -0400

    threat class template
    temple on what data to pull from the json file

--- ThreatAnalysisTool/src/Threat.java
+++ ThreatAnalysisTool/src/Threat.java
@@ -6,17 +6,16 @@ import java.util.List;
 class Threat
 {
 	private String type;
-	private String specVersion;
 	private String id;
+	private String created_by_ref;
 	private String created;
 	private String modified;
 	private String name;
 	private String description;
-	private List<ThreatActorTypes> thisListTypes;
-	private List<ThreatRoles> roles;
-	private List<ThreatGoals> goals;
-	private boolean isFamily;
-	private List<KillChain> killChainPhases;
+	private String external_reference;
+	private List<String> x_mitre_permissions_required;
+	private String spec_version;
+	private List<KillChain> kill_chain_phases;
 
 	Threat() {}
 
@@ -25,15 +24,15 @@ class Threat
 		return type;
 	}
 
-	String getVersion()
-	{
-		return specVersion;
-	}
-
 	String getID()
 	{
 		return id;
 	}
+
+	String getCreated_by_ref()
+	{
+		return created_by_ref;
+	}	
 
 	String getDateCreated()
 	{
@@ -55,28 +54,69 @@ class Threat
 		return description;
 	}
 
-	List<ThreatActorTypes> getActorTypes()
+	String getExernalRef()
 	{
-		return thisListTypes;
+		return external_reference;
 	}
 
-	List<ThreatRoles> getRoles()
+	List<String> getx_mitre_permissions_required()
 	{
-		return roles;
+		return x_mitre_permissions_required;
 	}
 
-	List<ThreatGoals> getGoals()
+	String getSpecVersion()
 	{
-		return goals;
-	}
-
-	boolean isThisAFamily()
-	{
-		return isFamily;
+		return spec_version;
 	}
 
 	List<KillChain> getKillChains()
 	{
-		return killChainPhases;
+		return kill_chain_phases;
 	}
+
+	void setType(String type)
+	{
+		this.type = type;
+	}
+
+	void setID(String id)
+	{
+		this.id = id;
+	}
+
+	void setCreated(String created)
+	{
+		this.created = created;
+	}
+
+	void setDateModified(String modified)
+	{
+		this.modified = modified;
+	}
+
+	void setName(String name)
+	{
+		this.name = name;
+	}
+
+	void setDescription(String description)
+	{
+		this.description = description;
+	}
+
+	void setExernalRef(String external_reference)
+	{
+		this.external_reference = external_reference;
+	}
+
+	void setx_mitre_permissions_required(List<String> x_mitre_permissions_required )
+	{
+		this.x_mitre_permissions_required =  x_mitre_permissions_required;
+	}
+
+	void setSpecVersion(String spec_version)
+	{
+		this.spec_version =  spec_version;
+	}
+
 }
