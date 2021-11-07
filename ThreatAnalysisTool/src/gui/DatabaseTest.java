6f88107a3fa999d1d52ffa38b16913fb4d96f23d
John-K97 <kenney97@students.rowan.edu>
2021-11-06 23:27:22 -0400

    addUser, checks that no user of same username exists before adding

--- ThreatAnalysisTool/src/gui/DatabaseTest.java
+++ ThreatAnalysisTool/src/gui/DatabaseTest.java
@@ -15,32 +15,40 @@ public class DatabaseTest {
 	final String DB_PASS = "VulturesA";
 
 	
-	public boolean addUser(String username, String password, int accessLevel) {
-		boolean userAdded = false;
-		Date date = new Date();
-		SimpleDateFormat dateFormatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-		String databaseQuery = "INSERT INTO user_credentials"
-				+ "(username, password, last_login, access_level)"
-				+ "VALUES"
-				+ "(\"" + username + "\", SHA1(\"" + password + "\"), \"" + dateFormatter.format(date) + "\", \"" + accessLevel + "\")";
-		try(Connection connection = DriverManager.getConnection(DB_URL, DB_USER, DB_PASS);
-				Statement statement = connection.createStatement();) {
-				statement.executeUpdate(databaseQuery);
-				statement.close();
-				connection.close();
-				userAdded = true;
-			} catch (SQLException e) {
-				e.printStackTrace();
-				//System.out.println("User Not Added");
-			}
+	public int addUser(String username, String password, int accessLevel) {
+		//return code -1 - error occurred (database-related)
+		//return code  1 - user added
+		//return code  2 - error: user name already exists
+		int returnCode = -1;
+		if (!userExists(username)) {
+			Date date = new Date();
+			SimpleDateFormat dateFormatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+			String databaseQuery = "INSERT INTO user_credentials"
+					+ "(username, password, last_login, access_level)"
+					+ "VALUES"
+					+ "(\"" + username + "\", SHA1(\"" + password + "\"), \"" + dateFormatter.format(date) + "\", \"" + accessLevel + "\")";
+			try(Connection connection = DriverManager.getConnection(DB_URL, DB_USER, DB_PASS);
+					Statement statement = connection.createStatement();) {
+					statement.executeUpdate(databaseQuery);
+					statement.close();
+					connection.close();
+					returnCode = 1;
+				} catch (SQLException e) {
+					e.printStackTrace();
+					//System.out.println("User Not Added");
+					returnCode = -1;
+				}
+		}else {
+			returnCode = 2;
+		}
 		
 		
-		return userAdded;
+		return returnCode;
 	}
 	
 	public int authenticateUser(String username, String password) {
 		//function will return  1 if user was authenticated
-		//function will return  2 if username/password combo was incorrect
+		//function will return  2 if user name/password combo was incorrect
 		//function will return -1 for any other error (db-related)
 		int returnCode = -1;
 		String databaseQuery = "SELECT * FROM user_credentials WHERE "
@@ -66,5 +74,28 @@ public class DatabaseTest {
 				e.printStackTrace();
 			}
 		return returnCode;
+	}
+	
+	private boolean userExists(String username) {
+		boolean exists = false;
+		String databaseQuery = "SELECT * FROM user_credentials WHERE username=\"" + username + "\"";
+		try(Connection connection = DriverManager.getConnection(DB_URL, DB_USER, DB_PASS);
+				Statement statement = connection.createStatement();
+				ResultSet resultSet = statement.executeQuery(databaseQuery);) {
+				
+				//Parse result set
+				if (resultSet.next()) {
+					//found existing user in database
+					exists = true;
+				}
+				
+				statement.close();
+				resultSet.close();
+				connection.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		
+		return exists;
 	}
 }
